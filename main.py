59350860ce0f6ccbad60727e0b0d16873146679c
IBungy <bcain4902@gmail.com>
2025-06-25 13:58:59 -0400

    fixed word count function

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def get_book_text(path_to_file):
 def word_count(string):
     big_string = string.split()
     num_words = len(big_string)
-    print("{num_words} words found in the document")
+    return num_words
+    
+
+words_in_book = word_count("books/frankenstein.txt")  
+print(f"{words_in_book} words found in the document")
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
