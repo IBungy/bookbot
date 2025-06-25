02e5b49db2abb65b0a13f64da1d8130a56d4a6d0
IBungy <bcain4902@gmail.com>
2025-06-25 14:04:15 -0400

    fixed word count again

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def word_count(string):
     num_words = len(big_string)
     return num_words
     
-
-words_in_book = word_count("books/frankenstein.txt")  
-print(f"{words_in_book} words found in the document")
+with open("books/frankenstein.txt", 'r') as f:
+    book_text = f.read()
+words_found_in_document = word_count(book_text)
+print(f"{words_found_in_document} words found in the document")
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
