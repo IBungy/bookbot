6e9887c1284897b053c8c528c2723e2911ba32e6
IBungy <bcain4902@gmail.com>
2025-06-25 13:25:24 -0400

    fixed return statement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-return file_contents
+    return file_contents
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
