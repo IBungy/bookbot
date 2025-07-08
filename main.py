6cc21b1eba722bd6601eaa19d1875c26dbeecf26
IBungy <bcain4902@gmail.com>
2025-07-08 14:48:14 -0400

    saved changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import word_count
 from stats import character_count
 from stats import sorted_list
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,10 +10,13 @@ def get_book_text(path_to_file):
 
 def main():
     print("============ BOOKBOT ============")
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
     words_found_in_document = word_count(book_text)
     characters_found = character_count(book_text)
-    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at ...")
     print("----------- Word Count ----------")
     print(f"Found {words_found_in_document} total words")
     print("--------- Character Count -------")
