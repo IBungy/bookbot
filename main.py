a7f4aae8d77d014e4b09d8a0f233277a60494df4
IBungy <bcain4902@gmail.com>
2025-07-08 13:43:09 -0400

    added character_count and imported

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from stats import word_count
+from stats import character_count
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
-
-from stats import word_count
     
 with open("books/frankenstein.txt", 'r') as f:
     book_text = f.read()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def word_count(string):
     big_string = string.split()
     num_words = len(big_string)
     return num_words
+
+def character_count(string):
+    characters = {}
+    lower = string.lower()
+    for char in lower:
+        if char not in characters:
+            characters[char] = 1
+        else:
+            characters[char] = characters[char] + 1
+    return characters
