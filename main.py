975c6037127dfcc8d0ec24188bdc37f67363a21c
IBungy <bcain4902@gmail.com>
2025-07-08 13:58:48 -0400

    changed dictionary print

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
-    
-with open("books/frankenstein.txt", 'r') as f:
-    book_text = f.read()
-words_found_in_document = word_count(book_text)
-print(f"{words_found_in_document} words found in the document")
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    words_found_in_document = word_count(book_text)
+    characters_found = character_count(book_text)
+    print(f"{words_found_in_document} words found in the document")
+    for char, count in list(characters_found.items())[:10]:
+        print(f"'{char}': {count}")
 
 if __name__ == "__main__":
     main()
